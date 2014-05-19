53456201d80577cc5814791e0617e6ac2c726a8b
Patrick Fong <patrickf3139@gmail.com>
2014-05-19 13:52:23 -0700

    updated comments and readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 CS61BL_Grading
 ==============
 
-The various python scripts for Linux that I wrote to facilitate and automate the grading of CS61BL assignments (summer of 2013). I had created a previous repo under the same name to store most of these same files but deleted it a year later when I realized I had pushed solutions to some of the homework.
+Here are the various python shell scripts that I wrote to facilitate and automate the grading of CS61BL assignments (summer of 2013). I had created a previous repo under the same name to store most of these same files but deleted it a later when I realized I had accidentally pushed solutions to some of the homework.
+
+"get-work using subprocess.py" is a mostly functional copy of get-work except using the newer python module to do the same work.
--- get-work.py
+++ get-work.py
@@ -1,14 +1,16 @@
 #! /usr/bin/python
 # Author: Patrick Fong
-# Updated: 08/22/2013
+# Updated: 05/19/2014
 
 import os
 import sys
 
 ROSTER = """\
-edit this string to contain your student's login and name
-format below
-cs61bl-xx FIRST_MIDDLE_LAST
+Edit this string to contain your students' logins and names, one student per line.
+Format below:
+cs61bl-x1 FIRST_MIDDLE_LAST1
+cs61bl-x2 FIRST_MIDDLE_LAST2
+...
 """
 
 HELPTEXT = """\
@@ -17,15 +19,15 @@ Usage: get-work.py DEADLINE ASSIGNMENT
        get-work.py -h
 
 get-work will create a directory named ASSIGNMENT. Within it, each submission for ASSIGNMENT \
-with timestamps <= DEADLINE will be retrieved into a directory named its submitter. DEADLINE \
-should be given in the form yyyymmddhhmm. Use the -n option to download all assignments, \
-ignoring deadline.
+with timestamps <= DEADLINE will be retrieved into a directory named after its submitter. \
+DEADLINE should be given in the form yyyymmddhhmm. Use the -n option to download all \
+assignments, ignoring deadline.
 """
 
 if __name__ == "__main__":
 	if sys.argv[1][0] == "-":
 		if sys.argv[1] == "-n":
-			#use a deadline that is effectively irrelevant
+			# use a deadline that is effectively irrelevant
 			deadline = "209904200000"
 			assignment = sys.argv[2]
 		elif sys.argv[1] == "-h":
@@ -44,13 +46,13 @@ if __name__ == "__main__":
 	os.chdir(assignment)
 	os.system("get-submissions " + assignment)
 
-	#maps logins to the timestamp of the latest subm unpacked for that login
+	# maps logins to the timestamp of the latest subm unpacked for that login
 	time_stamps = dict() 
 
-	#This loops processes each link, calling lookat on it if
-	#it is on your roster, is not late, but is later than the subm 
-	#already unpacked for that login. os.listdir does not output in
-	#a sorted manner so timestamps have to be tracked
+	# This loops processes each link, calling lookat on it if
+	# it is on your roster, is not late, but is later than the subm 
+	# already unpacked for that login. os.listdir does not output in
+	# a sorted manner so timestamps have to be tracked
 	for subm in os.listdir("."):
 	 	login = subm[0:9]
 	 	subm_date = subm[10:]
@@ -58,14 +60,14 @@ if __name__ == "__main__":
 	 		pass
 	 	elif subm_date > deadline:
 	 		os.system("echo " + subm.replace(".", " : ", 1) + " >> late")
-	 	#unpack subm if subm_date is later than that of the last subm for this login unpacked
+	 	# unpack subm if subm_date is later than that of the last subm for this login unpacked
 	 	elif subm_date > time_stamps.get(login, 0):
 	 		os.system("lookat -d " + login + " " + subm)
 	 		time_stamps[login] = subm_date
 	 	os.system("rm " + subm)
 
-	#Add to file grades only logins from roster whose 
-	#work you have called lookat on 
+	# Add to file grades only logins from roster whose 
+	# work you have called lookat on 
 	for line in ROSTER.split("\n"):
 		if line[0:9] in time_stamps:
 			os.system("echo " + line + ">> grades")
